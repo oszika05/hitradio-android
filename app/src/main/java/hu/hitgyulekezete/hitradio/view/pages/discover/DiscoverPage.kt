2ef7980d6eeb2b04e5ee4b4735d21648bb6d5135
Zsolt Oszlányi <zsolt.oscar@gmail.com>
2021-11-12 02:34:34 +0100

    discover page

--- app/src/main/java/hu/hitgyulekezete/hitradio/view/pages/discover/DiscoverPage.kt
+++ app/src/main/java/hu/hitgyulekezete/hitradio/view/pages/discover/DiscoverPage.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -17,15 +16,25 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import hu.hitgyulekezete.hitradio.audio.controller.AudioController
+import hu.hitgyulekezete.hitradio.audio.controller.AudioStateManager
+import hu.hitgyulekezete.hitradio.audio.controller.playPauseForSource
+import hu.hitgyulekezete.hitradio.audio.controller.sourcePlaybackState
 import hu.hitgyulekezete.hitradio.model.program.Episode
 import hu.hitgyulekezete.hitradio.model.program.Person
 import hu.hitgyulekezete.hitradio.model.program.Program
+import hu.hitgyulekezete.hitradio.model.program.asSource
+import hu.hitgyulekezete.hitradio.view.components.button.Button
+import hu.hitgyulekezete.hitradio.view.components.button.ButtonVariant
+import hu.hitgyulekezete.hitradio.view.components.episode.episodecard.EpisodeCard
+import hu.hitgyulekezete.hitradio.view.components.textfield.TextField
 
 
 @ExperimentalFoundationApi
 @Composable
 fun DiscoverPage(
     viewModel: DiscoverPageViewModel = hiltViewModel(),
+    audioController: AudioController,
     onAllProgramsClick: (search: String) -> Unit = {},
     onProgramClick: (program: Program) -> Unit = {},
     onAllPeopleClick: (search: String) -> Unit = {},
@@ -38,13 +47,19 @@ fun DiscoverPage(
     val episodes = viewModel.episodes.collectAsLazyPagingItems()
 
     LazyColumn {
-        item("header_title") {
-            Text("Keresés")
-        }
-        stickyHeader {
-            TextField(value = search, onValueChange = {
-                viewModel.search.compareAndSet(search, it)
-            })
+        item("header") {
+            Spacer(Modifier.height(16.dp))
+
+            TextField(
+                value = search,
+                onValueChange = {
+                    viewModel.search.compareAndSet(search, it)
+                },
+                label = "Keresés",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
         }
 
         item("programs") {
@@ -55,9 +70,10 @@ fun DiscoverPage(
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text("Műsorok")
-                    Text(
-                        "Összes",
-                        modifier = Modifier.clickable {
+                    Button(
+                        text = "Összes",
+                        variant = ButtonVariant.Ternary,
+                        onClick = {
                             onAllProgramsClick(search)
                         }
                     )
@@ -92,9 +108,10 @@ fun DiscoverPage(
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text("Személyek")
-                    Text(
-                        "Összes",
-                        modifier = Modifier.clickable {
+                    Button(
+                        text = "Összes",
+                        variant = ButtonVariant.Ternary,
+                        onClick = {
                             onAllPeopleClick(search)
                         }
                     )
@@ -129,13 +146,18 @@ fun DiscoverPage(
         items(episodes) { episode ->
             episode ?: return@items
 
-            Text(
-                episode.title,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .clickable {
-                        onEpisodeClick(episode)
-                    }
+            val playbackStateFlow = audioController.sourcePlaybackState(episode.asSource().id)
+            val playbackState by playbackStateFlow.collectAsState(AudioStateManager.PlaybackState.STOPPED)
+
+            EpisodeCard(
+                episode = episode,
+                playbackState = playbackState,
+                onClick = {
+                    onEpisodeClick(episode)
+                },
+                onPlayClick = {
+                    audioController.playPauseForSource(episode.asSource())
+                },
             )
         }
     }
