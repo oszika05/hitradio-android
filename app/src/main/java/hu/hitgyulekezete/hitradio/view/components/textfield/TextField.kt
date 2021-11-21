ed8534bcfb4aceed91b5600179f018b501011062
Zsolt Oszlányi <zsolt.oscar@gmail.com>
2021-11-21 16:47:24 +0100

    TextField modification

--- app/src/main/java/hu/hitgyulekezete/hitradio/view/components/textfield/TextField.kt
+++ app/src/main/java/hu/hitgyulekezete/hitradio/view/components/textfield/TextField.kt
@@ -1,25 +1,36 @@
 package hu.hitgyulekezete.hitradio.view.components.textfield
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hu.hitgyulekezete.hitradio.view.common.modifiers.coloredshadow.coloredShadow
+import hu.hitgyulekezete.hitradio.view.common.modifiers.coloredshadow.nowPlayingShadow
 import hu.hitgyulekezete.hitradio.view.common.preview.PreviewContainer
 import hu.hitgyulekezete.hitradio.view.components.label.Label
+import hu.hitgyulekezete.hitradio.view.layout.secondaryText
 
 @Composable
 fun TextField(
     value: String,
     onValueChange: (String) -> Unit,
     label: String? = null,
+    placeholder: String? = null,
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
@@ -35,15 +46,16 @@ fun TextField(
             Label(text = label, color = color)
             Spacer(modifier = Modifier.height(4.dp))
         }
+
         Box(
             Modifier
-                .fillMaxSize()
-                .border(
-                    width = 1.dp,
-                    color = color,
-                    shape = RoundedCornerShape(15)
-                )
-                .padding(vertical = 8.dp, horizontal = 8.dp)
+                .fillMaxWidth()
+                .height(42.dp)
+                .nowPlayingShadow()
+                .clip(RoundedCornerShape(50))
+                .background(MaterialTheme.colors.surface)
+                .padding(vertical = 10.dp, horizontal = 16.dp),
+            contentAlignment = Alignment.CenterStart,
         ) {
             BasicTextField(
                 value = value,
@@ -54,7 +66,20 @@ fun TextField(
                     .onFocusChanged {
                         isFocused = it.isFocused
                     },
+                singleLine = true,
+                maxLines = 1,
+                cursorBrush = SolidColor(MaterialTheme.colors.secondaryText),
             )
+
+            placeholder?.let { placeholder ->
+                if (value.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        color = MaterialTheme.colors.secondaryText,
+                        style = MaterialTheme.typography.body1,
+                    )
+                }
+            }
         }
 
     }
@@ -66,7 +91,7 @@ fun TextField(
 fun Preview_TextField() {
     PreviewContainer {
         Column {
-            var value by remember { mutableStateOf("") }
+            var value by remember { mutableStateOf("Test") }
             TextField(
                 value,
                 onValueChange = { value = it },
@@ -79,7 +104,8 @@ fun Preview_TextField() {
                 value,
                 onValueChange = { value = it },
                 label = "Name",
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = "Search"
             )
         }
 
